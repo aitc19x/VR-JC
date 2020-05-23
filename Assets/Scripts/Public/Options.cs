afd85a211cff309c1297c4ce13cb068bf93ba3fa
TURX <turx2003@gmail.com>
2020-05-24 02:25:53 +0800

    l10n, readme, enhanced WelcomeScene [1]

---
+++ Assets/Scripts/Public/Options.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{
+    public Canvas MainCanvas, OptionsCanvas;
+    public Dropdown dropdown;
+    public bool generateMenu = false;
+
+    IEnumerator Start()
+    {
+        yield return LocalizationSettings.InitializationOperation;
+
+        int selected = 0;
+
+        if (generateMenu) {
+            var options = new List<Dropdown.OptionData>();
+            for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
+            {
+                var locale = LocalizationSettings.AvailableLocales.Locales[i];
+                if (LocalizationSettings.SelectedLocale == locale)
+                    selected = i;
+                options.Add(new Dropdown.OptionData(locale.name));
+            }
+            dropdown.options = options;
+        } else {
+            
+            for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
+            {
+                var locale = LocalizationSettings.AvailableLocales.Locales[i];
+                if (LocalizationSettings.SelectedLocale == locale)
+                    selected = i;
+            }
+        }
+
+        dropdown.value = selected;
+        dropdown.onValueChanged.AddListener(LocaleSelected);
+    }
+
+    private static void LocaleSelected(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
+
+    public static void ExitApp()
+    {
+        System.Environment.Exit(0);
+    }
+
+    public void HideOptions() {
+        OptionsCanvas.gameObject.SetActive(false);
+        MainCanvas.gameObject.SetActive(true);
+    }
+}
--- Assets/Scripts/Scene/WelcomeScene.cs
+++ Assets/Scripts/Scene/WelcomeScene.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class WelcomeScene : MonoBehaviour
 {
+    public GameObject SphereView;
+    public Canvas MainCanvas, OptionsCanvas, CopyrightCanvas;
+
+    public void Start() {
+        InvokeRepeating("TurnBackground", 0.01f, 0.01f);
+    }
+
+    private void TurnBackground() {
+        Material mat = SphereView.GetComponent<Renderer>().material;
+        Vector2 vec = mat.GetTextureOffset("_MainTex");
+        vec.x += 0.0001f;
+        mat.SetTextureOffset("_MainTex", vec);
+    }
 
     public void ShowMenu()
     {
@@ -11,6 +25,17 @@ public class WelcomeScene : MonoBehaviour
 
     public void ShowCopyright()
     {
-        SceneManager.LoadScene("CopyrightScene");
+        MainCanvas.gameObject.SetActive(false);
+        CopyrightCanvas.gameObject.SetActive(true);
+    }
+
+    public void HideCopyright() {
+        CopyrightCanvas.gameObject.SetActive(false);
+        MainCanvas.gameObject.SetActive(true);
+    }
+
+    public void ShowOptions() {
+        MainCanvas.gameObject.SetActive(false);
+        OptionsCanvas.gameObject.SetActive(true);
     }
 }

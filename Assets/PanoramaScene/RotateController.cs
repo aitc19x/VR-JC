dc2bdd2aa3f1ea291c732c6e6650f02b92f16ac4
TURX <turx2003@gmail.com>
2020-06-03 00:36:09 +0800

    Fix compile error

--- Assets/PanoramaScene/RotateController.cs
+++ Assets/PanoramaScene/RotateController.cs
@@ -1,17 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class rotateController : MonoBehaviour
 {
     Vector3 center = new Vector3(0, 0, 0);
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         this.transform.RotateAround(center, Vector3.up, (Input.GetAxis("Mouse X") * 5));
